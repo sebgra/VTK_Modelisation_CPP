aec1fd9a744992d5a90fbe2295799cedab6e16c3
sebgra <sebastiengradit@hotmail.com>
2021-04-07 14:15:08 +0200

    Slider Added

--- src/StructuredPointsReader.cxx
+++ src/StructuredPointsReader.cxx
@@ -85,17 +85,17 @@ int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
 
+  QWidget fenetre;
+
   QVTKWidget *widget = new QVTKWidget;
   widget->resize( 500, 500 );
+
+  QSlider *slider = new QSlider(Qt::Horizontal,0);
 
   QVBoxLayout *layout = new QVBoxLayout(); //
 
   layout -> addWidget(widget);
-
-
-  QSlider slider(Qt::Horizontal,0); //
-
-  layout -> addWidget(&slider);
+  layout -> addWidget(slider);
 
   vtkSmartPointer<vtkSphereSource> sphereSource =
   vtkSmartPointer<vtkSphereSource>::New();
@@ -115,7 +115,47 @@ int main(int argc, char** argv)
 
 
   widget->GetRenderWindow()->AddRenderer( renderer );
-  widget->show();
+
+  fenetre.setLayout(layout);
+
+  fenetre.show();
+
+  // ==============================================
+
+
+
+
+  // QVTKWidget *widget = new QVTKWidget;
+  // widget->resize( 500, 500 );
+
+  // QVBoxLayout *layout = new QVBoxLayout(); //
+
+  // layout -> addWidget(widget);
+
+
+  // QSlider slider(Qt::Horizontal,0); //
+
+  // layout -> addWidget(&slider);
+
+  // vtkSmartPointer<vtkSphereSource> sphereSource =
+  // vtkSmartPointer<vtkSphereSource>::New();
+
+  // vtkSmartPointer<vtkPolyDataMapper> sphereMapper =
+  // vtkSmartPointer<vtkPolyDataMapper>::New();
+  // sphereMapper->SetInputConnection( sphereSource->GetOutputPort() );
+
+  // vtkSmartPointer<vtkActor> sphereActor =
+  // vtkSmartPointer<vtkActor>::New();
+  // sphereActor->SetMapper( sphereMapper );
+
+  // vtkSmartPointer<vtkRenderer> renderer =
+  // vtkSmartPointer<vtkRenderer>::New();
+  // renderer->AddActor( sphereActor );
+
+
+
+  // widget->GetRenderWindow()->AddRenderer( renderer );
+  // widget->show();
 
 
   app.exec();
